274c19c0da14ac71d1085aab32b49b82e38c0501
Mehedi Hasan Rafi <68476489+MehediRafi@users.noreply.github.com>
2023-08-04 13:20:58 +0600

    Add files via upload

---
+++ University-Bookstore/St.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University_Bookstore
+{
+    public class St
+    {
+
+        //public static string connection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Mehadi Hasan Shojib\Desktop\University-Bookstore\AdminDB\AdminloginDB.mdf;Integrated Security=True;Connect Timeout=30";
+        public static string connection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Semester 06\OOP--02 [Q]\Project\University-Bookstore\AdminDB\AdminloginDB.mdf;Integrated Security=True;Connect Timeout=30";
+
+    }
+}
